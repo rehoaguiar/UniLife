ff02846f9da638936338e2bd8e146e7e5da9e7a2
rehoaguiar <renata.palatinho@gmail.com>
2025-03-02 12:35:04 -0300

    Final das Atividades

--- Programação para Web/Atividade de Revisão 01/questao3.js
+++ Programação para Web/Atividade de Revisão 01/questao3.js
@@ -1,1 +1,23 @@
 // 3 - Crie um programa que pede ao usuário para digitar um número e, em seguida, exibe a tabuada desse número de 1 a 10 no formato de uma tabela. Exemplo de saída: Se o número escolhido por 5, a saída será: 5 x 1 = 5 5 x 2 = 10 5 x 3 = 15 ... 5 x 10 = 50
+
+function perguntarNumUser(){
+    let numUser = prompt('Digite um número: ')
+    return parseInt(numUser)
+}
+
+function verificarNum(numUser){
+    if(isNaN(numUser)){
+        console.log('Número inválido. Tente novamente.');
+        let novoNum = perguntarNumUser();
+        verificarNum(novoNum);      
+             // chama recursivamente com o novo número
+    } else {
+        console.log(`Tabuada do número ${numUser}`)
+        for (let i = 1; i <= 10; i++){
+            console.log(`${numUser} x ${i} = ${numUser * i}`)
+        }
+    }
+}
+
+let numero = perguntarNumUser();
+verificarNum(numero);
--- Programação para Web/Atividade de Revisão 01/questao5.js
+++ Programação para Web/Atividade de Revisão 01/questao5.js
@@ -1,1 +1,34 @@
 // 5 - Escreva um programa para encontrar a soma da série 1 + 11 + 1111 + ... n termos. Conforme a entrada a seguir: Exemplo da saída: Se o usuário digitar a quantidade de termos igual a 5, o resultado será: 1 + 11 + 111 + 1111 + 11111 A soma é: 12345
+
+function perguntarQuantidade(){
+    let numTermos = prompt('Digite o número de termos: ');
+    return parseInt(numTermos);
+}
+
+function calcularSerie(numTermos){
+    if(isNaN(numTermos)){
+        console.log('Valor inválido! Favor digite somente números');
+        let novoNumero = perguntarNum();
+        calcularSerie(novoNumero)
+
+    } else {
+        let soma = 0
+        let resultado = "";
+    
+        for (let i = 1; i <= numTermos; i++){
+            let termo = '1'.repeat(i);
+            soma += parseInt(termo);
+            resultado += termo;
+
+            if (i < numTermos){
+                resultado += ' + ';
+            }
+        }
+
+        console.log(resultado);
+        console.log("A soma é: " + soma);
+    }
+}
+
+let quantidade = perguntarQuantidade();
+calcularSerie(quantidade);
