6dcbd59d9afd47803f4d59a56683ab55f37d33d7
Renata Aguiar <you@example.com>
2025-06-15 11:56:59 -0300

    Testando / estudando reactjs

---
+++ Programação para Web/Projeto de Extensão/src/components/Inicio.js
@@ -0,0 +1,17 @@
+function Inicio() {
+  return (
+    <div id="inicio">
+      <h1>Eleve seu estilo de vida a outro nível com um treino de qualidade!</h1>
+      <p>
+        Olá! Sou Raquel, lorem lorem lorem lorem lotem lorem lorem lorem lorem lorem lotem lorem lorem lorem.
+      </p>
+      <button>Compre agora</button>
+
+      <div id="imagem-inicio">
+        <img src="/images/exemplo.jpg" alt="Descrição da imagem" />
+      </div>
+    </div>
+  );
+}
+
+export default Inicio;
